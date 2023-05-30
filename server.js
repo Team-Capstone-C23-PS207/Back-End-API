839b5231bad9010571ef3def2cfb2559505a0772
DhickyMahesya <dhivanipramana19@gmail.com>
2023-05-30 14:01:22 +0700

    Fixing bug scanner jenis sampah dari Model ML

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const tf = require('@tensorflow/tfjs-node');
+const fs = require('fs');
 const multer = require('multer');
 
 // Inisialisasi Express
@@ -9,72 +10,69 @@ app.use(bodyParser.json());
 
 // Konfigurasi multer untuk mengelola file yang diunggah
 const upload = multer({
-  dest: 'uploads/', // direktori penyimpanan file
+    dest: 'uploads/', // direktori penyimpanan file
 });
 
 // Mendefinisikan endpoint untuk menerima data gambar dan melakukan deteksi sampah
 app.post('/detect', upload.single('image'), async (req, res) => {
-  // Mendapatkan file gambar dari request
-  const imageFile = req.file;
+    // Mendapatkan file gambar dari request
+    const imageFile = req.file;
 
-  try {
-    // Melakukan deteksi sampah menggunakan model ML
-    const detectedWaste = await detectWaste(imageFile.path);
+    try {
+        // Melakukan deteksi sampah menggunakan model ML
+        const detectedWaste = await detectWaste(imageFile.path);
 
-    // Mengembalikan hasil deteksi sampah
-    res.json({ result: detectedWaste });
-  } catch (error) {
-    console.error('Terjadi kesalahan saat melakukan deteksi sampah:', error);
-    res.status(500).json({ error: 'Terjadi kesalahan saat melakukan deteksi sampah.' });
-  }
+        // Mengembalikan hasil deteksi sampah
+        res.json({ result: detectedWaste });
+    } catch (error) {
+        console.error('Terjadi kesalahan saat melakukan deteksi sampah:', error);
+        res.status(500).json({ error: 'Terjadi kesalahan saat melakukan deteksi sampah.' });
+    }
 });
 
-  const modelData = require('./tmp/tfjs_my_model/model.json');
-
+const modelPath = './tmp/tfjs_my_model/model.json';
 
 // Fungsi untuk melakukan deteksi sampah menggunakan model ML
 async function detectWaste(imagePath) {
-  // Mengimpor model ML dari file JSON
-  const model = await tf.loadLayersModel(`file://${modelData}`);
+    // Mengimpor model ML dari file JSON
+    const model = await tf.loadLayersModel(`file://${modelPath}`);
 
-  // Mempersiapkan gambar untuk deteksi
-  const image = await loadImage(imagePath);
-  const processedImage = await processImage(image);
+    // Mempersiapkan gambar untuk deteksi
+    const image = await loadImage(imagePath);
+    const processedImage = await processImage(image);
 
-  // Melakukan prediksi menggunakan model
-  const inputTensor = tf.tensor(processedImage);
-  const predictions = model.predict(inputTensor);
-  const predictedClass = predictions.argMax(1).dataSync()[0];
+    // Melakukan prediksi menggunakan model
+    const inputTensor = tf.expandDims(processedImage, 0); // Mengubah dimensi gambar ke bentuk yang diterima oleh model
+    const predictions = model.predict(inputTensor);
+    const predictedClass = predictions.argMax(1).dataSync()[0];
 
-  // Mengambil label yang sesuai dengan hasil prediksi
-  const labels = ['Plastik', 'Kertas', 'Logam', 'Kaca'];
-  const detectedWaste = labels[predictedClass];
+    // Mengambil label yang sesuai dengan hasil prediksi
+    const labels = ['AluCan', 'Glass', 'PET'];
+    const detectedWaste = labels[predictedClass];
 
-  return detectedWaste;
+    return detectedWaste;
 }
 
 // Fungsi untuk memuat gambar menggunakan TensorFlow.js
 async function loadImage(imagePath) {
-  const image = tf.node.decodeImage(await fs.promises.readFile(imagePath));
-  return image;
+    const imageBuffer = await fs.promises.readFile(imagePath); // Baca gambar sebagai buffer
+    const image = tf.node.decodeImage(imageBuffer); // Ubah buffer menjadi tf.Tensor menggunakan decodeImage()
+    return image;
 }
 
 // Fungsi untuk memproses gambar sebelum deteksi
 async function processImage(image) {
-  // Mengubah ukuran gambar menjadi yang sesuai dengan model
-  const resizedImage = tf.image.resizeBilinear(image, [224, 224]);
-  
-  // Normalisasi piksel gambar
-  const normalizedImage = resizedImage.div(tf.scalar(255));
+    // Mengubah ukuran gambar menjadi yang sesuai dengan model
+    const resizedImage = tf.image.resizeBilinear(image, [150, 150]);
 
-  // Mengubah dimensi gambar ke bentuk yang diterima oleh model
-  const processedImage = normalizedImage.expandDims();
+    // Normalisasi piksel gambar
+    const normalizedImage = resizedImage.div(255);
 
-  return processedImage;
+    return normalizedImage;
 }
 
 // Menjalankan server pada port yang ditentukan
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server berjalan pada http://localhost:${port}`);
+    console.log(`Server berjalan pada http://localhost:${port}`);
 });
