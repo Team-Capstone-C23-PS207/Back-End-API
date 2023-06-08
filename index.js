1e7838c3b55d3a228378276c48c12d2d309925ba
DhickyMahesya <dhivanipramana19@gmail.com>
2023-06-08 11:14:34 +0700

    memperbarui port ke 8080 v2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { app } = require('./server');
 
 // Menjalankan server pada port yang ditentukan
-const port = process.env.PORT || 8080; // Menggunakan port yang diberikan oleh variabel lingkungan PORT, atau port 8080 jika tidak ada
+const port = 8080; 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
