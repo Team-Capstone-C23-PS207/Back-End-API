58fbc471ff2651d371becea58c5528a973afe153
DhickyMahesya <dhivanipramana19@gmail.com>
2023-06-08 11:51:46 +0700

    mengembalikan di port 3000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { app } = require('./server');
 
 // Menjalankan server pada port yang ditentukan
-const port = 8080; 
+const port = 3000; 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
